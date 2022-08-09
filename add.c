1983b5d02a4691f8110dd22615defdc4650c45c5
Sachin Khatak <khataksachin00@gmail.com>
2022-08-09 12:21:21 +0530

    first commit

---
+++ add.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    int a, b;
+    printf("Enter number a");
+    scanf("%d", &a);
+    printf("Enter number a");
+    scanf("%d", &a);
+    return 0;
+}
---
+++ main.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main(int argc, char const *argv[])
+{
+    printf("hello World i am coming");
+    return 0;
+}
